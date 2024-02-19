4453d577b4809d0e18e57cbd0fbdc20b446e7c90
Avijit Das <avijitavi338895@gmail.com>
2024-02-19 21:16:33 +0000

    Updated

--- lib/widgets/widget_support.dart
+++ lib/widgets/widget_support.dart
@@ -9,4 +9,22 @@ class AppWidget {
       fontFamily: 'Poppins',
     );
   }
+
+  static TextStyle headlineTextFieldStyle() {
+    return TextStyle(
+      fontSize: 24.0,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Poppins',
+    );
+  }
+
+  static TextStyle lightTextFieldStyle() {
+    return TextStyle(
+      fontSize: 15.0,
+      color: Colors.black38,
+      fontWeight: FontWeight.w500,
+      fontFamily: 'Poppins',
+    );
+  }
 }
