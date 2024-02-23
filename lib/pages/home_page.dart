4fb94d264676fa10d0615bf269336bd190ba93d8
Avijit Das <avijitavi338895@gmail.com>
2024-02-23 23:44:44 +0600

    Updated

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,6 +16,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(),
       body: SingleChildScrollView(
         child: Container(
           margin: const EdgeInsets.only(
