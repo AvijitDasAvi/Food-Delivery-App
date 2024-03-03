7993bd2d331bf468e9942362082d4ac2adafdcc3
Avijit Das <avijitavi338895@gmail.com>
2024-03-03 20:22:01 +0600

    Updated

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/pages/home_page.dart';
+import 'package:food_delivery_app/widgets/widget_support.dart';
 
 class DetailsPage extends StatefulWidget {
   const DetailsPage({super.key});
@@ -9,12 +10,14 @@ class DetailsPage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsPage> {
+  int count = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         margin: const EdgeInsets.only(top: 25.0, left: 20.0, right: 20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             GestureDetector(
               onTap: () {
@@ -30,8 +33,72 @@ class _DetailsPageState extends State<DetailsPage> {
                 color: Colors.black,
               ),
             ),
-            Image.asset(
-              "images/salad2.png",
+            Center(
+              child: Image.asset(
+                "images/salad2.png",
+                width: MediaQuery.of(context).size.width / 1.5,
+                height: MediaQuery.of(context).size.height / 2,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Mediterraneam",
+                      style: AppWidget.semiBoldTextFieldStyle(),
+                    ),
+                    Text(
+                      "Chickpea Salad",
+                      style: AppWidget.boldTextFieldStyle(),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: IconButton(
+                    icon: const Icon(Icons.remove),
+                    color: Colors.white,
+                    onPressed: () {
+                      if (count > 1) {
+                        count--;
+                      }
+                      setState(() {});
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  width: 20.0,
+                ),
+                Text(
+                  "$count",
+                  style: AppWidget.semiBoldTextFieldStyle(),
+                ),
+                const SizedBox(
+                  width: 20.0,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: IconButton(
+                    icon: const Icon(Icons.add),
+                    color: Colors.white,
+                    onPressed: () {
+                      count++;
+                      setState(() {});
+                    },
+                  ),
+                )
+              ],
             )
           ],
         ),
