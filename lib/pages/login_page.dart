f26504612bbea23db862e6579b9638b790c7b5a7
Avijit Das <avijitavi338895@gmail.com>
2024-03-04 23:59:57 +0600

    Updated

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/widgets/widget_support.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,41 +13,104 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          child: Stack(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 2.5,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color.fromARGB(255, 245, 110, 73),
-                  Color.fromARGB(255, 228, 57, 5),
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color.fromARGB(255, 245, 110, 73),
+                    Color.fromARGB(255, 228, 57, 5),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin:
+                  EdgeInsets.only(top: MediaQuery.of(context).size.height / 3),
+              height: MediaQuery.of(context).size.height / 2,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(
+                    30.0,
+                  ),
+                  topRight: Radius.circular(
+                    30.0,
+                  ),
+                ),
+              ),
+              child: Text(''),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                top: 30.0,
+                left: 20.0,
+                right: 20.0,
+              ),
+              child: Column(
+                children: [
+                  Center(
+                    child: Image.asset(
+                      "images/logo.png",
+                      width: MediaQuery.of(context).size.width / 1.5,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Container(
+                      padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 2,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.white,
+                      ),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            "Login",
+                            style: AppWidget.headlineTextFieldStyle(),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: "Email",
+                              hintStyle: AppWidget.semiBoldTextFieldStyle(),
+                              prefixIcon: Icon(Icons.email_outlined),
+                              prefixIconColor: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              hintText: "Password",
+                              hintStyle: AppWidget.semiBoldTextFieldStyle(),
+                              prefixIcon: Icon(Icons.password_outlined),
+                              prefixIconColor: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               ),
-            ),
-          ),
-          Container(
-            margin:
-                EdgeInsets.only(top: MediaQuery.of(context).size.height / 3),
-            height: MediaQuery.of(context).size.height / 2,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(
-                  30.0,
-                ),
-                topRight: Radius.circular(
-                  30.0,
-                ),
-              ),
-            ),
-          ),
-        ],
-      )),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
