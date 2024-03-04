a30811891a7e05b504283f016252f1f3ea3ab0e1
Avijit Das <avijitavi338895@gmail.com>
2024-03-04 22:42:16 +0600

    Updated

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          child: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2.5,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 245, 110, 73),
+                  Color.fromARGB(255, 228, 57, 5),
+                ],
+              ),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
