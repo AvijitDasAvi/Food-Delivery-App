0b78913cafc3e9a95c4ec0680e755f46d8b7fde8
Avijit Das <avijitavi338895@gmail.com>
2024-03-12 02:09:02 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/pages/onboard_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "FOODIE",
-      home: OnboardPage(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "FOODIE",
+        home: FutureBuilder(
+          future: _initialization,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print("Error");
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              return const OnboardPage();
+            }
+            return const CircularProgressIndicator();
+          },
+        ));
   }
 }
--- lib/widgets/content_model.dart
+++ lib/widgets/content_model.dart
@@ -8,17 +8,17 @@ class OnboardingContent {
 
 List<OnboardingContent> contents = [
   OnboardingContent(
-    discription: "Pick your food from our menu\n    More than 35 times",
+    discription: "Pick your food from our menu\n         More than 35 times",
     image: "images/screen1.png",
-    title: "Select from our\n    Best menu",
+    title: "Select from our\n     Best menu",
   ),
   OnboardingContent(
-    discription: "You can pay cash on delivery\n  Card payment is available",
+    discription: "You can pay cash on delivery\n   Card payment is available",
     image: "images/screen2.png",
     title: "Easy and online payment",
   ),
   OnboardingContent(
-    discription: "Delivery your food at your\n Doorstep",
+    discription: "Delivery your food at your\n                 Doorstep",
     image: "images/screen3.png",
     title: "Quick delivery at Your Doorstep",
   )
