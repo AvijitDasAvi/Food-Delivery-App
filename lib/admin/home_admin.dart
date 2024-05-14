52b27dd7c01091aca225d44e1b1571e1f9376d4d
Avijit Das <avijitavi338895@gmail.com>
2024-05-14 21:48:50 +0600

    Home Admin Complete

--- lib/admin/home_admin.dart
+++ lib/admin/home_admin.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/admin/add_food.dart';
+import 'package:food_delivery_app/widgets/widget_support.dart';
 
 class HomeAdmin extends StatefulWidget {
   const HomeAdmin({super.key});
@@ -10,6 +12,65 @@ class HomeAdmin extends StatefulWidget {
 class _HomeAdminState extends State<HomeAdmin> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                "Home Admin",
+                style: AppWidget.headlineTextFieldStyle(),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AddFood()));
+              },
+              child: Material(
+                elevation: 10.0,
+                borderRadius: BorderRadius.circular(10),
+                child: Center(
+                  child: Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(6.0),
+                          child: Image.asset(
+                            "images/food.jpg",
+                            height: 80,
+                            width: 80,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 90.0,
+                        ),
+                        Text(
+                          "Add Food Items",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
